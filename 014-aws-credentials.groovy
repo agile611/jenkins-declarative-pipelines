ea0e1de1f6c83e473b89d29fd58a6a2d7fc71722
Guillem Hernandez Sola <guillem@agile611.com>
2025-12-17 12:41:19 +0100

    Added all

--- 014-aws-credentials.groovy
+++ 014-aws-credentials.groovy
@@ -1,19 +1,13 @@
 pipeline{
     agent any
     environment {
-        AWS_ACCESS_KEY_ID     = credentials('aws-access-key-id')
-        AWS_SECRET_ACCESS_KEY = credentials('aws-secret-access-key')
-        withCredentials([[
-            $class: 'AmazonWebServicesCredentialsBinding',
-            credentialsId: 'AWS_KEY' //ID en el que se almacenan las credenciales en Jenkins con el nombre que hemos puesto
-        ]]) 
+        AWS_ACCESS_KEY_ID = credentials('AWS_KEY')
     }
 
     stages {
         stage('Show AWS Credentials') {
             steps {
                 sh 'echo "AWS Access Key ID: $AWS_ACCESS_KEY_ID"'
-                sh 'echo "AWS Secret Access Key: $AWS_SECRET_ACCESS_KEY"'
             }
         }
     }
