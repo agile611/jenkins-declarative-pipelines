8d58eef6b348ad1ea62e73ffde3b372c5d6747b2
Guillem Hernandez Sola <guillem@agile611.com>
2025-12-17 12:47:21 +0100

    Added all

--- 014-aws-credentials.groovy
+++ 014-aws-credentials.groovy
@@ -4,9 +4,10 @@ pipeline{
     stages {
         stage('Show AWS Credentials') {
             steps {
-                withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'AWS_KEY', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {
-                sh 'echo "AWS Access Key ID: $AWS_ACCESS_KEY_ID"'
-                sh 'echo "AWS Secret Access Key: $AWS_SECRET_ACCESS_KEY"'
+                withCredentials([aws(accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'AWS_KEY', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY')]) {  
+                    sh 'echo "AWS Access Key ID: $AWS_ACCESS_KEY_ID"'
+                    sh 'echo "AWS Secret Access Key: $AWS_SECRET_ACCESS_KEY"'
+                }
             }
         }
     }
