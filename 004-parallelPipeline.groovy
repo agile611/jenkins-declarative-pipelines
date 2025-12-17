225a80bc84ea875cf13643317b567416b15211e1
Guillem Hernandez Sola <guillem@agile611.com>
2025-12-17 11:29:12 +0100

    Added new html reports

--- 004-parallelPipeline.groovy
+++ 004-parallelPipeline.groovy
@@ -1,35 +1,34 @@
 pipeline {
     agent none
     stages {
-            stage("Primer") {
-                steps {
-                    parallel(
-                            first: {
-                                    echo "First branch"
-                                },
-                            second: {
-                                    echo "Second branch"
-                                }
-                            )
-                }
+        stage('Setup') {
+            steps {
+                echo 'Setting up the environment...'
             }
-            stage("Second") {
-                steps {
-                    parallel(
-                            first: {
-                                    echo "First branch"
-                                },
-                            second: {
-                                    echo "Second branch"
-                                }
-                            third: {
-                                    echo "Third branch"
-                                },
-                            fourth: {
-                                    echo "Fourth branch"
-                                }
-                            )
+        }
+        stage('Parallel Stages') {
+            parallel {
+                stage('Stage 1') {
+                    steps {
+                        echo 'Executing Stage 1'
                     }
                 }
+                stage('Stage 2') {
+                    steps {
+                        echo 'Executing Stage 2'
+                    }
+                }
+                stage('Stage 3') {
+                    steps {
+                        echo 'Executing Stage 3'
+                    }
+                }
+            }
         }
+        stage('Teardown') {
+            steps {
+                echo 'Cleaning up the environment...'
+            }
+        }
+    }
 }
