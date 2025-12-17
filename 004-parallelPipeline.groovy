e75d8890e955975d023a4f514415b744acf69419
Guillem Hernandez Sola <guillem@agile611.com>
2025-12-17 11:27:44 +0100

    Added new html reports

--- 004-parallelPipeline.groovy
+++ 004-parallelPipeline.groovy
@@ -1,17 +1,35 @@
 pipeline {
     agent none
     stages {
-        stage("foo") {
-            steps {
-                parallel(
-                         first: {
-                                 echo "First branch"
-                             },
-                         second: {
-                                 echo "Second branch"
-                             }
-                         )
+            stage("Primer") {
+                steps {
+                    parallel(
+                            first: {
+                                    echo "First branch"
+                                },
+                            second: {
+                                    echo "Second branch"
+                                }
+                            )
+                }
             }
+            stage("Second") {
+                steps {
+                    parallel(
+                            first: {
+                                    echo "First branch"
+                                },
+                            second: {
+                                    echo "Second branch"
+                                }
+                            third: {
+                                    echo "Third branch"
+                                },
+                            fourth: {
+                                    echo "Fourth branch"
+                                }
+                            )
+                    }
+                }
         }
-    }
 }
