5fbb1893dd75df6dbb912ac711c43e76c247057d
Guillem Hernandez Sola <guillem@agile611.com>
2025-12-17 13:25:59 +0100

    Added terraform flow

--- 015-terraform-flow.groovy
+++ 015-terraform-flow.groovy
@@ -12,5 +12,15 @@ pipeline{
                 sh 'terraform init'                
             }
         }
+        stage('Apply Terraform'){
+            steps{
+                //Genera el plan y lo guarda en un archivo binario 'tfplan'
+                // -out es vital para guardar el plan en un archivo
+                sh 'terraform plan -out=tfplan'
+
+                //Opcional: Mostrar el plan en texto para que el usuario vea los cambios que se van a aplicar
+                sh 'terraform show -no-color tfplan'
+            }   
+        }   
     }
 }
