5d93da48d96f0f70fc6d486f81207b21b23017c9
Guillem Hernandez Sola <guillem@agile611.com>
2025-12-18 10:44:18 +0100

    Trying ansible

--- 016-ansible.groovy
+++ 016-ansible.groovy
@@ -19,7 +19,7 @@ pipeline {
 
         stage('Install ansible'){
             steps{
-                sh 'python -m pip install --upgrade pip'
+                sh 'python3 -m pip install --upgrade pip'
                 sh 'pip install ansible ansible-lint'
             }
         }
