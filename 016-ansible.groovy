6b1f4aef185c9bc452a93dcd6f8424374431ca6a
Guillem Hernandez Sola <guillem@agile611.com>
2025-12-18 10:40:41 +0100

    Trying ansible

--- 016-ansible.groovy
+++ 016-ansible.groovy
@@ -1,10 +1,13 @@
+// Mejor instalar Ansible en el nodo Jenkins 
+// Si se quiere usar el plugin de Ansible para Jenkins se tiene que buscar el Zip de Instalación y luego instalarlo en el nodo Jenkins  
+// NOTA IMPORTANTE: Hace falta crear un environment de python para que la tool de Ansible funcione correctamente
 pipeline {
     agent {
-            label 'pelado'
+            label 'pelado' // Nodo donde está instalado Ansible
     }
-    
-    tools{
-        ansible 'ansible'
+
+    tools {
+        ansible 'ansible' // Nombre de la instalación de Ansible en Jenkins
     }
 
     stages{
